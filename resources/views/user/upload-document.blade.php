8cf48e7c96c0bcb1d9fef97f12c2739155ea4646
Minh Hieu <minhhieuleviet@gmail.com>
2025-11-28 16:20:20 +0700

    Change to active mode n8n

--- resources/views/user/upload-document.blade.php
+++ resources/views/user/upload-document.blade.php
@@ -212,7 +212,7 @@
         formData.append("file", file, file.name);
 
         // Sử dụng localhost vì browser gọi từ phía client
-        const n8nWebhookURL = "https://n8n.mhieu.io.vn/webhook-test/file-upload";
+        const n8nWebhookURL = "https://n8n.mhieu.io.vn/webhook/file-upload";
 
         try {
             const resp = await fetch(n8nWebhookURL, {
